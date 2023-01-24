5681caa2d83b6b854d423e708a76013403b7c353
Maciej Skorski <maciej.skorski@gmail.com>
2023-01-24 23:11:17 +0100

    multidim distribution of brownian mod 1

--- src/oscillatory_trng/functions.py
+++ src/oscillatory_trng/functions.py
@@ -3,7 +3,8 @@ import mpmath as mpm
 
 def jtheta3(z,q):
     """
-        Jacobi Theta-3 function, as defined in Abramowitz, M. & Stegun, I. A. (1964), Handbook of Mathematical Functions with Formulas, Graphs, and Mathematical Tables
+        Jacobi Theta-3 function.
+        The scaling convention follows the definition in Abramowitz, M. & Stegun, I. A. (1964), "Handbook of Mathematical Functions... ".
     """
     def jtheta3_fn(z,q):
         jtheta3_fn = lambda z,q: mpm.jtheta(n=3,z=z,q=q)
@@ -12,3 +13,18 @@ def jtheta3(z,q):
     
     return jtheta3_fn(z,q)
 
+
+def browian_mod1_logp(mu,sigma,t,y):
+    """Evaluate the log-probability of the Brownian motion modulo 1.
+    Args:
+        mu: drift parameter of the Brownian motion
+        sigma: drift parameter of the Brownian motion
+        t: array of time points, non-decreasign and non-negative
+        y: array of points to evaluate density, between 0 and 1
+    """
+    t_diff = np.diff(np.append(0,t))
+    y_diff = np.diff(np.append(0,y))
+    z = np.pi*(y_diff-mu*t_diff)
+    q = np.exp(-2*np.pi**2*t_diff*sigma**2)
+    return np.log(jtheta3(z,q)).sum()
+    
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,9 +1,11 @@
-from scipy.stats import norm
 import numpy as np
-from oscillatory_trng.functions import jtheta3
+from scipy.stats import norm
+from scipy.stats import multivariate_normal
+from scipy.special import logsumexp
+
 
 def test_1d_probability():
-    """ Test the formula expressing normal distribution modulo 1 in terms of Jacobi theta function. """
+    """Test the formula expressing normal distribution modulo 1 in terms of Jacobi theta function."""
     from oscillatory_trng.functions import jtheta3
 
     # try example values
@@ -11,13 +13,34 @@ def test_1d_probability():
     mu = 0.1
     y = 0.05
     # by definition
-    pdf = norm(mu,sigma).pdf
-    ks = np.arange(-100,100)
-    val_def = pdf(y+ks).sum()
+    dist = norm(mu, sigma)
+    ks = np.arange(-100, 100)
+    logp_true = logsumexp(dist.logpdf(y + ks))
     # by theorem
-    z = np.pi*(y-mu)
-    q = np.exp(-2*sigma**2*np.pi**2)
-    val_thm = jtheta3(z,q)
+    z = np.pi * (y - mu)
+    q = np.exp(-2 * sigma**2 * np.pi**2)
+    logp_formula = np.log(jtheta3(z, q))
 
-    np.testing.assert_almost_equal(val_def, val_thm, decimal=5)
-    
+    np.testing.assert_almost_equal(logp_true, logp_formula)
+
+
+def test_browian_mod1_logp():
+    """Test the formula for finite-dimentional probability of Brownian motion modulo 1"""
+    from oscillatory_trng.functions import browian_mod1_logp
+
+    # try example values
+    sigma = 0.25
+    mu = 0.01
+    t = np.array([1, 2.5, 3])
+    y = np.array([0.2, 0.5, 0.3])
+    mean = mu * t
+    cov = sigma**2 * np.clip(t, 0, t.reshape(-1, 1))
+    dist = multivariate_normal(mean, cov)
+    ks = np.arange(-10, 10)
+    ks = np.meshgrid(ks, ks, ks)
+    ks = np.stack(ks, axis=-1)  # (N,N,N,3)
+    logp_true = logsumexp(dist.logpdf(y + ks))
+
+    logp_formula = browian_mod1_logp(mu, sigma, t, y)
+
+    np.testing.assert_almost_equal(logp_true, logp_formula)
