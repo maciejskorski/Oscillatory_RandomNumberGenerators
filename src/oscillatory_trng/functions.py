bae1b70e7d560f5257fccee65451d7756499a677
Maciej Skorski <maciej.skorski@gmail.com>
2023-01-26 01:45:03 +0100

    support of logp for more axes

--- src/oscillatory_trng/functions.py
+++ src/oscillatory_trng/functions.py
@@ -6,12 +6,12 @@ def jtheta3(z,q):
         Jacobi Theta-3 function.
         The scaling convention follows the NIST definition (dlmf.nist.gov) and Abramowitz, M. & Stegun, I. A. (1964), "Handbook of Mathematical Functions... ".
     """
-    def jtheta3_fn(z,q):
+    def fn(z,q):
         jtheta3_fn = lambda z,q: mpm.jtheta(n=3,z=z,q=q)
         jtheta3_fn = np.vectorize(jtheta3_fn,otypes=(np.cfloat,))
         return jtheta3_fn(z,q)
     
-    return jtheta3_fn(z,q)
+    return fn(z,q)
 
 
 def brownian_mod1_logp(mu,sigma,t,y):
@@ -19,12 +19,14 @@ def brownian_mod1_logp(mu,sigma,t,y):
     Args:
         mu: drift parameter of the Brownian motion
         sigma: drift parameter of the Brownian motion
-        t: array of time points, non-decreasign and non-negative
-        y: array of points to evaluate density, between 0 and 1
+        t: 1-D array of time points; non-decreasing and positive
+        y: array of points to evaluate density; values between 0 and 1, first dimension matches the length of t.
     """
     t_diff = np.diff(np.append(0,t))
-    y_diff = np.diff(np.append(0,y))
+    pads_dim = [(1,0)]+(len(y.shape)-1)*[(0,0)]
+    y = np.pad(y,pads_dim,mode='constant')
+    y_diff = np.diff(y,axis=0)
     z = np.pi*(y_diff-mu*t_diff)
     q = np.exp(-2*np.pi**2*t_diff*sigma**2)
-    return np.log(jtheta3(z,q)).sum()
+    return np.log(jtheta3(z,q)).sum(0)
     
